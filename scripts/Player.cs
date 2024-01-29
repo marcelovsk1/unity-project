94ac99a0cddcab521b606179100e938096a48d87
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-29 15:18:24 -0300

    working on player

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,3 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class Player : MonoBehaviour {
+  public float forceMultiplier = 5f;
+  public float maximumVelocity = 2f;
+}
