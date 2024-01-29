745bdceae4672826097154cc2202ee60cb006a91
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-29 14:53:22 -0300

    working on

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -1,3 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public GameObject hazardPrefab;
+  // Start is called before the first frame update
+}
