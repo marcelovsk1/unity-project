3b928c8b2a18d8405daa0b16128ab45a680dfc07
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-29 15:10:22 -0300

    working on hazard

--- scripts/Hazard.cs
+++ scripts/Hazard.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 public class Hazard : MonoBehaviour {
 
   Vector3 rotation;
-
+  private void Start() {
+   var xRotation = Random.Range(0.2f, 1f);
+   rotation = new Vector3(1,0);
+  }
 }
