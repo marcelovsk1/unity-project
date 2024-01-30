721b4cfa608632f982f944ba80cb5003ba97a1b8
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-30 13:31:38 -0300

    back to working on gamemanager

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
   public GameObject hazardPrefab;
   // Start is called before the first frame update
   public int maxHazardsToSpawn = 3;
+  public TMPro.TextMeshPro scoreText;
   void Start()
   {
     StartCoroutine(SpawnHazards());
