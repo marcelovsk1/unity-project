62979c3643020451732ec6dacbda02be06f4fdac
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-30 13:27:51 -0300

    working on coin

--- scripts/coin.cs
+++ scripts/coin.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class coin : MonoBehaviour {
-
+public class coin : MonoBehaviour
+{
+  //star is called before the first frame update
+  Vector3 rotation;
+  void Start()
+  {
+    var zRotation = Random.Range(0.1f, 0.3f);
+  }
 }
