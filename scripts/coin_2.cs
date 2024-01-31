44abcba9131314b60a8128483fcf51574add2dba
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-31 13:19:55 -0300

    working on coin2

--- scripts/coin_2.cs
+++ scripts/coin_2.cs
@@ -7,4 +7,8 @@ public class coin_2 : MonoBehaviour
   // Start is called before the first frame update
   Vector3 rotation;
   void Start()
+  {
+    var zRotation = Random.Range(0.01f, 0.05f);
+    rotation = new Vector3(0, 0, -zRotation);
+  }
 }
